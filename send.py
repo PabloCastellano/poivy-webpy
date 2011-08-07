173197c538b11e7712ca38f3511598665875593b
Pablo Castellano <pablog@anche.no>
2011-08-08 01:03:19 +0200

    Added GPLv3+ license and headers + AUTHORS,CONTRIBUTORS files

--- send.py
+++ send.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# send.py - This file is part of poivy-webpy - 8/August/2011
+# Copyright (C) 2011 Pablo Castellano <pablo@anche.no>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#       
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#               
+
+__version__ = 1.0
+__author__ = "Pablo Castellano <pablo@anche.no>"
+__license__ = "GNU GPLv3+"
 
 import web
 
